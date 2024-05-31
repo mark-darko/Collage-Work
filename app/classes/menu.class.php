a8fd25cf06eba78872ec91acc228f20c7bc315f1
MARK.DARKO <darko@markdarko.site>
2024-05-31 13:29:14 +0300

    обновление меню(регистрация, авторизация)
    1.доделать validate data практическая 5
    2.сделать роли в бд
    сделать
    3.сделать комментарии(отображение коммениариев на комментарии)

--- app/classes/menu.class.php
+++ app/classes/menu.class.php
@@ -3,15 +3,17 @@
         private $menu;
         private $active_link;
         private $response;
+        private $user;
 
         /**
          * @param array $menu
          */
-        public function __construct(array $menu, string $active_link, Response $response)
+        public function __construct(array $menu, string $active_link, Response $response, User $user)
         {
             $this->menu = $menu;
             $this->active_link = $active_link;
             $this->response = $response;
+            $this->user = $user;
         }
 
         public function get_menu ()
@@ -26,8 +28,11 @@
             $links = '';
 
             foreach ($this->menu as $item) {
+                if (($item["link"] == '/app/login.php' || $item["link"] == '/app/register.php') && $this->user->id) continue;
+                if ($item["link"] == '/app/logout.php' && !$this->user->id) continue;
+
                 if ($this->active_link == $item["link"])
-                    $links .= '<li class="colorlib-active"><a href="' . $this->response->getLink($item["link"]) .'">' . $$item["title"] . '</a></li>';
+                    $links .= '<li class="colorlib-active"><a href="' . $this->response->getLink($item["link"]) .'">' . $item["title"] . '</a></li>';
                 else
                     $links .= '<li><a href="' . $this->response->getLink($item["link"]) .'">' . $item["title"] . '</a></li>';
             }
--- app/initialize.php
+++ app/initialize.php
@@ -6,7 +6,7 @@
     $db = new Mysql($dbConfig);
     $user = new User($request, $db);
     $response = new Response($user);
-    $menu = new Menu($menu_array, parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH), $response);
+    $menu = new Menu($menu_array, parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH), $response, $user);
     $post = new Post($user);
 
     $menu_html = $menu->get_menu();

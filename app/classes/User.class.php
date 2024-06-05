fe9ec7394caf660e16ff4ef1a1957018c8bfb999
MARK.DARKO <darko@markdarko.site>
2024-06-05 13:40:54 +0300

    обновление сайта
    1.доделать validate data практическая 5
    2. mind map карта
    3. добавить в проект всю документацию
    4. перепроверить весь код и провести тестирование
    5. создать тест кейсы
    6. написать отчет
    7. сделать проверку на доступ к удалению комментария в классе

--- app/classes/User.class.php
+++ app/classes/User.class.php
@@ -234,10 +234,8 @@
         private function isAdmin()
         {
             $user_role = $this->db->queryAssoc("SELECT roles.name AS role_name
-            FROM `{$this->tableName}`
-            JOIN `roles` ON users.role_id = roles.id
-            WHERE users.id='{$this->id}'")['role_name'];
-            //var_dump($user_role);
+            FROM users
+            JOIN roles ON users.role_id = roles.id WHERE users.id='{$this->id}'")['role_name'];
 
             return $user_role == "admin" ? true : false;
         }
